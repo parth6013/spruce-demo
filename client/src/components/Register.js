55aab73e43c347bd5408a825b52b20092fb3d429
TheGreatFellow <shreyas42000@gmail.com>
2022-04-08 00:24:01 +0530

    Stylized Frontend ✨

--- client/src/components/Register.js
+++ client/src/components/Register.js
@@ -1,7 +1,8 @@
 import React from "react";
 import {useHistory, Link} from "react-router-dom";
 import axios from "axios";
-
+import '../App.css';
+import enterIcon from '../assets/enter-icon.png';
 function Register(props){
 
     const{
@@ -18,6 +19,7 @@ function Register(props){
     let history = useHistory();
 
     const signUp = async(e) =>{
+        console.log("signed up")
         e.preventDefault();
 
         try{
@@ -41,14 +43,22 @@ function Register(props){
     return(
         <div>
              <form>
-                <label for="fname">Name:</label><br></br>
-                <input type="text" value = {name} onChange ={(e)=>setName(e.target.value)} id="fname" name="fname"/><br></br>
-                <label for="lname">Twitter_Id</label><br></br>
-                <input type="text" id="lname" value={twitterId} onChange={(e)=>setTwitterId(e.target.value)} name="lname"/>
-                <label for="Password">Password</label><br></br>
-                <input type="password" id="Password" value={password} onChange={(e)=>setPassword(e.target.value)} name="lname"/>
-                <button onClick={signUp}>Register.</button>
-                <Link to={"/login"}><button>Login</button></Link>
+                <input type="text" value = {name} onChange ={(e)=>setName(e.target.value)} id="fname" name="fname" placeholder="Name" className="input-field" />
+               <br></br>
+               <br></br>
+                <input type="text" id="lname" value={twitterId} onChange={(e)=>setTwitterId(e.target.value)} name="lname" placeholder="Twitter Id" className="input-field" />
+                <br></br>
+                <br></br>
+                <input type="password" id="Password" value={password} onChange={(e)=>setPassword(e.target.value)} name="lname" placeholder="Password" className="input-field" />
+                <br></br>
+                <br></br>
+                <Link to={"/login"}><a className="redirect-link">Already have an account? Log in</a></Link>
+                <br></br>
+                <br></br>
+                <button class="primary-button" onClick={signUp}>
+                        <div class="button-text">Register</div>
+                        <img src={enterIcon} alt="enter" class="button-icon" />
+                    </button>
             </form> 
         </div>
     )
